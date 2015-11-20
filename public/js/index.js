320871a6c74ea51a8d0d1bddf223a06fe3f98a96
M. Maxwell Watson <hi@mmwtsn.com>
2015-11-19 22:33:41 -0500

    Create CommentBox component

---
+++ public/js/index.js
@@ -0,0 +1,16 @@
+/* global React, ReactDOM */
+
+var CommentBox = React.createClass({
+  render: function () {
+    return (
+      <div className='commentBox'>
+        Hello, world! I am a CommentBox.
+      </div>
+    )
+  }
+})
+
+ReactDOM.render(
+  <CommentBox />,
+  document.getElementById('content')
+)
