f0ce7b0abef8cb5fbb0cd94cdd4bcdbf51c48ba2
M. Maxwell Watson <hi@mmwtsn.com>
2015-11-19 23:31:33 -0500

    Fetch, reload comments from local data store

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,4 @@
-/* global React, ReactDOM */
-
-var data = [
-  {id: 1, author: 'Maxwell Watson', text: 'Hello world!'},
-  {id: 2, author: 'Maxwell Watson', text: 'Hallo Welt.'}
-]
+/* global $, React, ReactDOM */
 
 var CommentList = React.createClass({
   render: function () {
@@ -34,11 +29,34 @@ var CommentForm = React.createClass({
 })
 
 var CommentBox = React.createClass({
+  loadCommentsFromServer: function () {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function (data) {
+        this.setState({data: data})
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.error(this.props.url, status, err.toString())
+      }.bind(this)
+    })
+  },
+
+  getInitialState: function () {
+    return {data: []}
+  },
+
+  componentDidMount: function () {
+    this.loadCommentsFromServer()
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval)
+  },
+
   render: function () {
     return (
       <div className='commentBox'>
         <h1>Comments!</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     )
@@ -60,6 +78,6 @@ var Comment = React.createClass({
 })
 
 ReactDOM.render(
-  <CommentBox data={data} />,
+  <CommentBox url='/api/comments' pollInterval={1000} />,
   document.getElementById('content')
 )
